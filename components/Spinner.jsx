2893d069b88e90190d90cd81f6f534989c1eb9d4
mclancerlot <ldmcguir13+github@gmail.com>
2024-02-22 11:05:23 -0800

    Completed Database, API Routes & Property Components

--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -7,7 +7,7 @@ const override = {
   margin: '100px auto',
 };
 
-const LoadingPage = ({ loading }) => {
+const Spinner = ({ loading }) => {
   return (
     <ClipLoader
       color='#3b82f6'
@@ -19,4 +19,4 @@ const LoadingPage = ({ loading }) => {
     />
   );
 };
-export default LoadingPage;
+export default Spinner;
