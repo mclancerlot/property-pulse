8082d9ed5bfa3da314a9008d7eb69dc13afb6a4b
mclancerlot <ldmcguir13+github@gmail.com>
2024-02-28 12:18:35 -0800

    remove console log

--- components/FeaturedProperties.jsx
+++ components/FeaturedProperties.jsx
@@ -6,8 +6,6 @@ const FeaturedProperties = async () => {
     showFeatured: true,
   });
 
-  console.log(properties);
-
   return (
     properties.length > 0 && (
       <section className='bg-blue-50 px-4 pt-6 pb-10'>
